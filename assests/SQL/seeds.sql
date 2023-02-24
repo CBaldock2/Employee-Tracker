3df2f0ca64ea62fb56557cf656b63ca632eec12d
Christopher Baldock <christophermbaldock@gmail.com>
2023-02-24 16:04:37 -0600

    fixes manager issue of all employees appearing instead of only managers

--- assests/SQL/seeds.sql
+++ assests/SQL/seeds.sql
@@ -1,23 +1,19 @@
 INSERT INTO department (name)
 VALUES  ('Legal'),
-        ('Upper Level'),
         ('Supply'),
         ('Support'),
         ('Marketing');
 
 INSERT INTO role (title, salary, department_id)
-VALUES  ('Lawyer', 120000, 1),
-        ('Supply Associate', 55000, 3),
-        ('Marketing Analyst', 60000, 5),
-        ('Customer Service', 50000, 4),
-        ('Manager', 200000, 2);
+VALUES  ('Supply Manager', 125000, 2),
+        ('Marketing Manager', 100000, 4),
+        ('Support Manager', 125000, 3),
+        ('Lawyer', 120000, 1);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES  ('Bill', 'Ding', 1, 1),
-        ('Jo', 'King', 2, 2),
-        ('Stan', 'Still', 3, 3),
-        ('Ema', 'Lee', 2, 2),
-        ('Willie', 'Leak', 4, 4),
-        ('David', 'Pitts', 4, 4),   
+VALUES  ('Bill', 'Ding', 1, NULL),
+        ('Jo', 'King', 2, NULL),
+        ('Stan', 'Still', 3, NULL),
+        ('David', 'Pitts', 4, NULL); 
 
 
